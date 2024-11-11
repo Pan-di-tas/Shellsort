a2f3124220c8f55e186b4bef92b5c4e6c29d254a
Abdiel <maganaabdiel02@gmail.com>
2024-11-11 11:58:44 -0600

    Update Shellsort.py

--- Shellsort.py
+++ Shellsort.py
@@ -1,21 +1,26 @@
 class Tortuga:
     def shell_sort(self, arr):
         n = len(arr)
-        gap = n // 2
-        
+        gap = n // 2 
         while gap > 0:
             for i in range(gap, n):
                 temp = arr[i]
                 j = i
+                print("\nPosición inicial:")
+                print(f"Elemento actual: arr[{i}] = {arr[i]}")
                 while j >= gap and arr[j - gap] > temp:
+                    print(f"\nCambio de posición:")
+                    print(f"arr[{j}] = arr[{j - gap}] -> {arr[j - gap]} reemplaza {arr[j]}")
+                    
                     arr[j] = arr[j - gap]
                     j -= gap
                 arr[j] = temp
+            print(f"\nReducción del intervalo a la mitad: {gap} -> {gap // 2}")
             gap //= 2
-
+            
     def Arreglo(self):
         arr = []
-        Limite = int(input("Dame el tamaño del arreglo: "))
+        Limite = int(input("\nDame el tamaño del arreglo: "))
         for _ in range(Limite):
             Num = int(input("Ingresa un número: "))
             arr.append(Num)
@@ -23,6 +28,6 @@ class Tortuga:
 
 # Ejecución
 key = Tortuga()
-arr = key.Arreglo()  # Guardamos el arreglo creado por el usuario
-key.shell_sort(arr)  # Ordenamos el arreglo usando Shell Sort
-print("Arreglo ordenado:", arr)
+arr = key.Arreglo() 
+key.shell_sort(arr)
+print("\nArreglo ordenado:", arr)
